96a6716dad06805630119dcf8340017ae4ef7dc3
Edin <edin.lemes2004@gmail.com>
2025-11-14 21:07:01 +0100

    getRolesList

--- backend/Hotel.API/Controllers/User/RolesController.cs
+++ backend/Hotel.API/Controllers/User/RolesController.cs
@@ -2,20 +2,28 @@
 using Hotel.Application.Modules.Auth.Commands.Logout;
 using Hotel.Application.Modules.Auth.Commands.Refresh;
 using Hotel.Application.Modules.Auth.Roles.Commands.Create;
+using Hotel.Application.Modules.Auth.Roles.Querries.GetList;
 using Microsoft.AspNetCore.Mvc;
 
 namespace Hotel.API.Controllers.User
 {
     [ApiController]
     [Route("api/roles")]
-    public sealed class RolesController(IMediator mediator) : ControllerBase
+    public sealed class RolesController(ISender sender) : ControllerBase
     {
         [HttpPost("addRole")]
         [AllowAnonymous]
 
         public async Task<ActionResult<int>> Role([FromBody] CreateRolesCommand command, CancellationToken ct)
         {
-            return Ok(await mediator.Send(command, ct));
+            return Ok(await sender.Send(command, ct));
+        }
+        [HttpGet("listRoles")]
+        [AllowAnonymous]
+        public async Task<PageResult<ListRolesQueryDto>> List([FromQuery] ListRolesQuery query, CancellationToken ct)
+        {
+            var result = await sender.Send(query, ct);
+            return result;
         }
 
     }
---
+++ backend/Hotel.Application/Modules/Auth/Roles/Querries/GetList/ListRolesQuery.cs
@@ -0,0 +1,5 @@
+﻿namespace Hotel.Application.Modules.Auth.Roles.Querries.GetList;
+public sealed class ListRolesQuery : BasePagedQuery<ListRolesQueryDto>
+{
+    public string? Search { get; set; }
+}
