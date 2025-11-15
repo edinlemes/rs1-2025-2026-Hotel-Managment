0e5c47920b06773221bd06b563ddb7aa739451f3
Edin <edin.lemes2004@gmail.com>
2025-11-15 23:37:34 +0100

    getbyId handler

---
+++ backend/Hotel.Application/Modules/Auth/Queries/GetById/UserGetByIdQueryDto.cs
@@ -0,0 +1,16 @@
+﻿using Hotel.Domain.Entities.Users;
+
+namespace Hotel.Application.Modules.Auth.Queries.GetById;
+
+public sealed class UserGetByIdQueryDto
+{
+    public required int UserId { get; set; }
+    public string UserName { get; set; } = string.Empty;
+    public string UserEmail { get; set; } = string.Empty;
+    public RolesDto Role { get; set; } = new RolesDto();
+}
+public sealed class RolesDto
+{
+    public int RoleId { get; set; }
+    public string RoleName { get; set; } = string.Empty;
+}
