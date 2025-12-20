fa65aa6fc0f5572029484360b6d680140ceb9de2
Edin <edin.lemes2004@gmail.com>
2025-12-20 17:50:48 +0100

    added html code for register-form

--- Hotel.frontend/src/app/modules/auth/register-form/register-form.component.ts
+++ Hotel.frontend/src/app/modules/auth/register-form/register-form.component.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core';
+import { RegularExpressionLiteral } from '@angular/compiler';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-register-form',
@@ -6,6 +7,27 @@ import { Component } from '@angular/core';
   templateUrl: './register-form.component.html',
   styleUrls: ['./register-form.component.scss'],
 })
-export class RegisterFormComponent {
+export class RegisterFormComponent implements OnInit {
+
+  ngOnInit(): void {
+    const tabLogin = document.getElementById("tabLogin");
+    const tabRegister = document.getElementById("tabRegister");
+    const loginForm = document.getElementById("loginForm");
+    const registerForm = document.getElementById("registerForm");
+
+    tabLogin?.addEventListener("click", () => {
+      loginForm?.classList.remove("hidden");
+      registerForm?.classList.add("hidden");
+      tabLogin?.classList.add("active");
+      tabRegister?.classList.remove("active");
+    });
+
+    tabRegister?.addEventListener("click", () => {
+      registerForm?.classList.remove("hidden");
+      tabRegister?.classList.add("active");
+      loginForm?.classList.add("hidden");
+      tabLogin?.classList.remove("active");
+    });
+  }
 
 }
